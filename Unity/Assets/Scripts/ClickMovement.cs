54566c664b37e7ad69789acba3bb206b39e2b8ed
PedroHSilva <pedrohsilva.dev@gmail.com>
2022-02-10 04:22:26 -0400

    PathFinding added

---
+++ Unity/Assets/Scripts/ClickMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMovement : MonoBehaviour
+{
+    //private RaycastHit raycastHit;
+    //private Camera camera;
+    
+
+    private void Awake()
+    {
+        
+
+
+    }
+    void Update()
+    {
+        //moveCharacter();
+        //mousePosition.Set()
+    }
+
+    
+}
--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,39 @@ using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Transform moveDestinationTransform;
+    //[SerializeField] private Transform moveDestinationTransform;
+    [SerializeField] Camera camera;
+    [SerializeField] GameObject sphere;
     private NavMeshAgent navMeshAgent;
+    private InputActions inputActions;
+    public Vector2 mousePositionV2;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        //camera = GetComponent<Camera>();
+        inputActions = new InputActions();
+        inputActions.PlayerActionMap.Enable();
+        //mousePosition = inputActions.PlayerActionMap.MousePosition.ReadValue<Vector3>();
+
+
     }
 
     void Update()
     {
-        navMeshAgent.destination = moveDestinationTransform.position; 
+        navMeshAgent.destination = sphere.transform.position;
+        mousePositionV2 = inputActions.PlayerActionMap.MousePosition.ReadValue<Vector2>();
+    }
+
+    public void moveCharacter()
+    {
+        var ray = camera.ScreenPointToRay(mousePositionV2);
+        RaycastHit raycastHit;
+
+        if(Physics.Raycast(ray, out raycastHit))
+        {
+            print($"Atingiu {raycastHit.transform.name} em {raycastHit.point}");
+            sphere.transform.position = raycastHit.point;
+        }
     }
 }
